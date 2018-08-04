b55ffbeadcdddb56731aac92114bbd1e94d20b3b
David Palmquist <dpalmquist@Davids-MacBook-Pro.local>
2018-08-04 10:35:52 -0400

    finished variables

--- VariablesPluaralSight/src/Main.java
+++ VariablesPluaralSight/src/Main.java
@@ -2,5 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        float floatVal = 1.0f;
+        double doubleVal = 4.0d;
+        byte byteVal = 7;
+        short shortVal = 7;
+        long longVal = 5;
+
+        short result1 = byteVal;
+
+        System.out.println("Success");
     }
 }
